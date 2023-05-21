f639ef4c287e5a5007e7924a5e9c5908bc9efa56
melmesay <mohamed.elmasary123@gmail.com>
2023-05-22 01:41:06 +0300

    Update eks cluster configs

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -1,8 +1,1 @@
 data "aws_availability_zones" "azs" {}
-data "aws_eks_cluster" "saurus" {
-  name = module.eks.cluster_arn
-}
-
-data "aws_eks_cluster_auth" "saurus-auth" {
-  name = module.eks.cluster_arn
-}
